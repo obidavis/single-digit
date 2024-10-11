5832f1cc629816d90835be3b998daf948d0e1a80
Obi Davis <obgdavis@gmail.com>
2024-10-11 16:00:58 +0100

    remove count from generator page and added loading overlay

--- frontend/src/pages/Generator.tsx
+++ frontend/src/pages/Generator.tsx
@@ -14,6 +14,7 @@ import {
   Tile,
   Column,
   Content,
+  Loading,
 
 } from "@carbon/react";
 import {
@@ -23,7 +24,7 @@ import {
 import { SudokuGameState } from "../models/Sudoku";
 import { useState } from "react";
 import { Puzzle } from "../models/SudokuAPI";
-import { SudokuCard } from "../components/SudokuCard";
+import { SudokuCard } from "../components/GameCard";
 
 interface GeneratorResults {
   puzzles: Puzzle[];
@@ -38,7 +39,8 @@ export const GeneratorPage = () => {
   const handleSubmit = (event: any) => {
     event.preventDefault();
     const form = event.target;
-    const data = new FormData(form);
+    const formData = new FormData(form);
+    const body = Object.fromEntries(formData.entries());
 
     setLoading(true);
     setError(null);
@@ -46,8 +48,10 @@ export const GeneratorPage = () => {
 
     fetch('/api/generate', {
       method: 'POST',
-      body: data,
-      
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
     }).then(response => response.json())
       .then(data => {
         setResults(data);
@@ -57,33 +61,34 @@ export const GeneratorPage = () => {
       .catch(error => setError(error));
   }
   return (
-      <Stack gap={10}>
-        <Form onSubmit={handleSubmit} >
-          <Stack gap={5}>
-            <Select
-              id="difficulty"
-              name="difficulty"
-              labelText="Difficulty"
-              helperText="Select the difficulty of the puzzle"
-              defaultValue="easy"
-            >
-              <SelectItem value="easy" text="Easy" />
-              <SelectItem value="moderate" text="Moderate" />
-              <SelectItem value="tough" text="Tough" />
-              <SelectItem value="random" text="Random" />
-            </Select>
-            <NumberInput id="seed" name="seed" label="seed" defaultValue={-1} min={-1} />
-            <NumberInput id="count" name="count" label="count" defaultValue={1} min={0} />
-            <Button type="submit">Generate</Button>
-          </Stack>
-        </Form>
-        {loading && <p>Loading...</p>}
-        {error && <p>Error: {error}</p>}
-        {results && (
-          results.puzzles.map((puzzle, i) => (
-            <SudokuCard key={i} puzzle={puzzle} />
-          ))
-        )}
-      </Stack>
+    <>
+    <Loading active={loading} withOverlay/>
+    <Stack gap={10}>
+      <Form onSubmit={handleSubmit} >
+        <Stack gap={5}>
+          <Select
+            id="difficulty"
+            name="difficulty"
+            labelText="Difficulty"
+            helperText="Select the difficulty of the puzzle"
+            defaultValue="easy"
+          >
+            <SelectItem value="easy" text="Easy" />
+            <SelectItem value="moderate" text="Moderate" />
+            <SelectItem value="tough" text="Tough" />
+            <SelectItem value="random" text="Random" />
+          </Select>
+          <NumberInput allowEmpty id="seed" name="seed" label="seed" defaultValue={0} min={0} />
+          <Button type="submit">Generate</Button>
+        </Stack>
+      </Form>
+      {error && <p>Error: {error}</p>}
+      {results && (
+        results.puzzles.map((puzzle, i) => (
+          <SudokuCard key={i} puzzle={puzzle} />
+        ))
+      )}
+    </Stack>
+    </>
   );
 }
